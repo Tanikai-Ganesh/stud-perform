5c2cd0b63a765e00f1d886c1562214586c95a8c0
Tanikai-Ganesh <tanikaiceg02@gmail.com>
2025-02-28 13:24:09 +0530

    Model Deployment

--- application.py
+++ application.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, render_template,jsonify
 import random  # For demo purposes; replace with actual ML model
+import numpy as np
+import pandas as pd
+import os
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application = Flask(__name__)
 
@@ -13,20 +18,37 @@ def predict_datapoint():
         return render_template('home.html')
     if request.method == 'POST':
         # Get form data
-        gender = request.form.get('gender')
-        ethnicity = request.form.get('ethnicity')
-        parental_education = request.form.get('parental_level_of_education')
-        lunch = request.form.get('lunch')
-        test_prep = request.form.get('test_preparation_course')
+
+        gender = request.form.get('gender'),
+        ethnicity = request.form.get('ethnicity'),
+        parental_education = request.form.get('parental_level_of_education'),
+        lunch = request.form.get('lunch'),
+        test_prep = request.form.get('test_preparation_course'),
+        reading_score=float(request.form.get('writing_score')),
+        writing_score=float(request.form.get('reading_score'))
 
         # Basic validation
-        if not all([gender, ethnicity, parental_education, lunch, test_prep]):
+        if not all([gender, ethnicity, parental_education, lunch, test_prep,reading_score,writing_score]):
             return jsonify({'error': 'All fields are required'}), 400
 
-        # Demo prediction (replace with actual ML model logic)
-        predicted_score = random.randint(0, 100)
+        data = CustomData(
+                gender,ethnicity,parental_education,lunch,test_prep,reading_score,writing_score
+        )
         
-        return jsonify({'prediction': predicted_score})
+
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+
+        # Demo prediction (replace with actual ML model logic)
+        predicted_score = predict_pipeline.predict(pred_df)
+
+        print("after Prediction")
+        
+        return jsonify({'prediction': predicted_score[0]})
     
     return jsonify({'error': 'Method not allowed'}), 405
 
